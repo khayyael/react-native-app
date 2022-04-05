0bbbcb6a1b33a28bad69018e844dbab09bb01187
khadija <khadija.mjd007@gmail.com>
2022-04-05 14:34:49 +0200

    NavigationCommit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';;
 import MainScreen from './screens/main';
 import AuthScreen from './screens/auth';
 import { RootStackParamList } from './screens/RootStackPrams';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-const Stack = createStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
