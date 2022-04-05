2f7846f9f3410ebabd891044b5f8b6c2faffbc68
user <user@user.com>
2022-04-05 12:16:53 +0000

    get data from api github

--- App.tsx
+++ App.tsx
@@ -1,14 +1,43 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+
 
 export default function App() {
+
+const [users , setUsers] = useState([])
+const [loading , setLoading] = useState(true)
+
+useEffect(() => {
+  const getUsers = async () => {
+    try {
+      await axios.get("https://api.github.com/users")
+      .then(res => {
+          setUsers(res.data)
+          setLoading(true);
+      })
+
+            } catch (error) {
+        console.log(error);
+    }
+    setLoading(false)
+} 
+getUsers();
+}, []) 
+
+
   return (
     <View style={styles.container}>
-      <Text>Hamza Boulman Khadija Majid Kharrachi Khayya</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+       data={users}
+       renderItem={({item}) => <Text>{item.login}</Text>}
+      />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
