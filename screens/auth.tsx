e18bb2c5e5a6fc6e3ad97dafe0bccfcce3f88de5
khadija <khadija.mjd007@gmail.com>
2022-04-05 13:56:21 +0200

    FirstCommit

---
+++ screens/auth.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import { RootStackParamList } from './RootStackPrams';
+import { StackNavigationProp } from 'react-navigation-stack/lib/typescript/src/vendor/types';
+import { useNavigation } from '@react-navigation/native';
+
+type authScreenProp = StackNavigationProp<RootStackParamList, 'Auth'>;
+
+function AuthScreen() {
+  const navigation = useNavigation<authScreenProp>();
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Auth Screen</Text>
+      <Button title="Login" onPress={() => navigation.navigate('Main')} />
+    </View>
+  );
+}
+
+export default AuthScreen;
